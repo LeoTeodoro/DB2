32443fe8326a095333393332e0c1c58bc1a41bc5
LeoTeodoro <leoteoddoro18@gmail.com>
2023-11-24 19:25:22 -0300

    Projeto Final BDll

--- projetoLigasFut/main.py
+++ projetoLigasFut/main.py
@@ -4,33 +4,18 @@ from teamCRUD import Team
 from cli import TeamCLI
 
 db = Database("bolt://3.234.224.239:7687", "neo4j", "copies-tumble-spacers")
-db.drop_all()
+# db.drop_all()
 
 team_db = Team(db)
 league_db = League(db)
 
-# cli = TeamCLI(team_db)
-# cli.run()
-
-# Criando times
-# team_db.create("Flamengo", 1895, 42000000)
-# team_db.create("Palmeiras", 1914, 20225600)
-# team_db.create("Real Madrid", 1902, 8000000)
-# team_db.create("Manchester City", 1894, 30600000)
-# team_db.create("Chelsea", 1905, 22000000)
-
-# # Atualizando time
-# team_db.update("Flamengo", 42500000)
-
-# # Deletando time
-# team_db.delete("Chelsea")
-
-# # Retornando time
-# print("Time: ")
-# team_db.read("Real Madrid")
+cli = TeamCLI(team_db)
+cli.run()
 
 # Criando liga
-league_db.create(["Flamengo", "Palmeiras", "Real Madrid"], "Mundial de Clubes")
+league_db.create(
+    ["Flamengo", "Palmeiras", "Real Madrid", "Manchester City"], "Mundial de Clubes"
+)
 
 # Atualizando uma liga
 league_db.update("Mundial de Clubes", "Mundial de Clubes da Fifa")
